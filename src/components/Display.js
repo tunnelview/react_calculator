65b6c0ca9e27f4d01fa866cf6b0c2fcec2d0b11e
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-03-26 13:46:49 +1100

    Add or condition to textToDisplay on line 6

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,8 +3,7 @@ import React from "react";
 export const Display = ({ textToDisplay }) => {
   return (
     <div className="result" id="result">
-      {textToDisplay}
-      0.00
+      {textToDisplay || "0.00"}
     </div>
   );
 };
